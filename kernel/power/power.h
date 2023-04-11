be628fbe5c4677e3be3aca63e0d76eebdf9481b9
Dark-Matter7232 <me@const.eu.org>
2023-04-11 18:36:40 +0530

    kernel/power: fix permission mask for `power_attr_tmp()`
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>

--- kernel/power/power.h
+++ kernel/power/power.h
@@ -91,7 +91,7 @@ static struct kobj_attribute _name##_attr = {	\
 static struct kobj_attribute _name##_attr = {	\
 	.attr   = {				\
 		.name = __stringify(_name),	\
-		.mode = 0666,			\
+		.mode = 0644,			\
 	},					\
 	.show   = _name##_show,			\
 	.store  = _name##_store,		\
